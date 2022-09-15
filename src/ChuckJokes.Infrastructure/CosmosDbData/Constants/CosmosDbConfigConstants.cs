7a859e52b6615231da82ad157760a4482a5d0de1
motemood <motemood@gmail.com>
2022-09-15 11:13:33 +0200

    Clean-up

--- src/ChuckJokes.Infrastructure/CosmosDbData/Constants/CosmosDbConfigConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ChuckJokes.Infrastructure.CosmosDbData.Constants
-{
-    public class CosmosDbContainerConstants
-    {
-        // TODO : consider retrieving this from appsettings using IOptions, instead of defining it as a constant
-        public const string CONTAINER_NAME_TODO = "Todo";
-    }
-}
